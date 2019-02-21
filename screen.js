3b14343953b20cfcfd8da776054c5aa2ca26293c
Dominic Desimini <ddesimini@fortressinfosec.com>
2019-02-21 11:54:04 -0700

    removing part-3 code to get back to part-2

--- screen.js
+++ screen.js
@@ -52,50 +52,16 @@ class PlayScreen {
   }
   
   enter() { 
-    let map = [];
-    for (var x = 0; x < 80; x++) {
-        // Create the nested array for the y values
-        map.push([]);
-        // Add all the tiles
-        for (var y = 0; y < 24; y++) {
-            map[x].push(nullTile);
-        }
-    }
-    var generator = new ROT.Map.Cellular(80, 24);
-    generator.randomize(0.5);
-    var totalIterations = 3;
-    // Iteratively smoothen the map
-    for (var i = 0; i < totalIterations - 1; i++) {
-        generator.create();
-    }
-    // Smoothen it one last time and then update our map
-    generator.create(function(x,y,v) {
-        if (v === 1) {
-            map[x][y] = floorTile;
-        } else {
-            map[x][y] = wallTile;
-        }
-    });
-    // Create our map from the tiles
-    this._map = new Map(map);
+    console.log("Entered start screen");
    };
   exit() { console.log("Exited play screen."); };
   render(display) {
-    for (var x = 0; x < this._map.getWidth(); x++) {
-      for (var y = 0; y < this._map.getHeight(); y++) {
-          // Fetch the glyph for the tile and render it to the screen
-          var glyph = this._map.getTile(x, y).getGlyph();
-          display.draw(x, y,
-              glyph.getChar(), 
-              glyph.getForeground(), 
-              glyph.getBackground());
-      }
-  }
+    // Render the Text for this screen.
+    display.drawText(3,5, "%c{red}%b{white}This game is so much fun!");
+    display.drawText(4,6, "Press [Enter] to win, or [Esc] to lose!");
   };
   handleInput(inputType, Game) {
-    console.log(inputType);
     if (inputType.sequence === '\r') {
-      console.log(inputType);
         Game.switchScreen(this.winScreen);
     } else if (inputType.sequence === '\u001b') {
         Game.switchScreen(this.loseScreen);
